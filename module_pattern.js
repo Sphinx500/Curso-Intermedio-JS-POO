cd0d8eea139f14f888ea73b5881c4b69ccabef5c
Sphinx500 <1719210003@utectulancingo.edu.mx>
2021-11-01 13:10:39 -0600

    Module pattern y namespaces: propiedades privadas en JavaScript

---
+++ module_pattern.js
@@ -0,0 +1,101 @@
+function isObject(subject) {
+    return typeof subject == "object";
+}
+
+function isArray(subject) {
+    return Array.isArray(subject);
+}
+
+function deepCopy(subject) {
+    let copySubject;
+
+    const subjectIsObject = isObject(subject);
+    const subjectIsArray = isArray(subject);
+
+    if (subjectIsArray) {
+        copySubject = [];
+    } else if (subjectIsObject) {
+        copySubject = {};
+    } else {
+        return subject;
+    }
+
+    for (key in subject) {
+        const keyIsObject = isObject(subject[key]);
+
+        if (keyIsObject) {
+            copySubject[key] = deepCopy(subject[key]);
+        } else {
+            if (subjectIsArray) {
+                copySubject.push(subject[key]);
+            } else {
+                copySubject[key] = subject[key];
+            }
+        }
+    }
+
+    return copySubject;
+}
+
+// const studentBase = {
+//   name: undefined,
+//   email: undefined,
+//   age: undefined,
+//   approvedCourses: undefined,
+//   learningPaths: undefined,
+//   socialMedia: {
+//     twitter: undefined,
+//     instagram: undefined,
+//     facebook: undefined,
+//   },
+// };
+
+function requiredParam(param) {
+    throw new Error(param + " es obligatorio");
+}
+
+function createStudent({
+    name = requiredParam("name"),
+    email = requiredParam("email"),
+    age,
+    twitter,
+    instagram,
+    facebook,
+    approvedCourses = [],
+    learningPaths = [],
+} = {}) {
+    const private = {
+        "_name": name,
+    };
+
+    const public = {
+        email,
+        age,
+        approvedCourses,
+        learningPaths,
+        socialMedia: {
+            twitter,
+            instagram,
+            facebook,
+        },
+        readName() {
+            return private["_name"];
+        },
+        changeName(newName) {
+            private["_name"] = newName;
+        },
+    };
+
+    Object.defineProperty(public, "readName", {
+        writable: false,
+        configurable: false,
+    });
+    Object.defineProperty(public, "changeName", {
+        writable: false,
+        configurable: false,
+    });
+
+    return public;
+}
+
+const juan = createStudent({ email: "juanito@frijoles.co", name: "Juanito" });
