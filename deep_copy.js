dc547209d301601319ab300cef5745a608435f9c
Sphinx500 <1719210003@utectulancingo.edu.mx>
2021-11-03 11:09:09 -0600

    modificacion a deep copy

--- deep_copy.js
+++ deep_copy.js
@@ -1,14 +1,14 @@
-const obj1 = {
-    a: "a",
-    b: "b",
-    c: {
-        d: "d",
-        e: "e",
-    },
-    editA() {
-        this.a = "AAAAA";
-    }
-};
+// const obj1 = {
+//     a: "a",
+//     b: "b",
+//     c: {
+//         d: "d",
+//         e: "e",
+//     },
+//     editA() {
+//         this.a = "AAAAA";
+//     }
+// };
 
 //Si es un objeto
 function isObject(subject) {
@@ -27,7 +27,7 @@ function deepCopy(subject) {
 
     const subjectIsObject = isObject(subject)
     const subjectIsArray = isArray(subject)
- 
+
     // inicializamos la variable copySubject segun su tipo de dato
     if (subjectIsArray) {
         copySubject = [];
@@ -39,11 +39,11 @@ function deepCopy(subject) {
 
     // Empieza la recursividad
     for (key in subject) {
-         // propiedad de nuestro elemento
+        // propiedad de nuestro elemento
         const keyIsObject = isObject(subject[key]);
         if (keyIsObject) {
             // Si la propiedad es un objeto, se vuelve a llamar a deepCopy para volver a hacer las asignaciones por cada una de las propiedades de los objetos.
-            copySubject[key] = deepCopy(subject[key]) 
+            copySubject[key] = deepCopy(subject[key])
         } else {
             if (subjectIsArray) {
                 copySubject.push(subject[key])
@@ -55,3 +55,24 @@ function deepCopy(subject) {
 
     return copySubject;
 }
+
+//modelo de estudiantes sin prototipos, aplicando la abstraccion
+const studentBase = {
+    name: undefined,
+    email: undefined,
+    age: undefined,
+    approvedCourses: undefined,
+    learningPaths: undefined,
+    socialMedia: {
+        twitter: undefined,
+        instagram: undefined,
+        facebook: undefined,
+    },
+};
+
+const juan = deepCopy(studentBase);
+//Evita que podamos borrar las propiedades de los objetos
+Object.seal(juan);
+//Revisa si el objeto tiene las propiedades protegidas
+Object.isSealed(juan);
+  // juan.name = "Juanito";
